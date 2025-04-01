faccddcfa67a81b314416f5f098f6288d299be69
Sumar Illya <illasumar002@gmail.com>
2025-04-01 17:58:32 +0300

    Delete lab08/prj directory

--- lab08/prj/Modules/Sumar.cpp
+++
@@ -1,6 +0,0 @@
-#include <cmath>
-
-using namespace std;
-double s_calculation(double , double , double ) {
-    return pow(x, 2) - pow(y, 3) + sqrt(abs(pow(z, 2) * exp(x))) / sqrt(12 * x + (pow(y, 2) - M_PI * sqrt(z)));
-}
--- lab08/prj/Modules/Sumar.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUMAR_H
-#define SUMAR_H
-
-double s_calculation(double x, double y, double z);
-
-#endif
